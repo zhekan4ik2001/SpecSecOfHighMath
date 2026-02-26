d67f918c52b63b7dc9d45d21023365ea561baf8b
zhekan4ik2001 <zh.plastinin2015@gmail.com>
2026-02-26 23:10:10 +0300

    Исправлена 1 лабораторная работа под 6 вариант

--- lab1.py
+++ lab1.py
@@ -3,19 +3,28 @@ from scipy import optimize
 import matplotlib.pyplot as plt
 
 def equation_1_fx(x):
-    return 8 * np.cos(x) - x
+    if x < -7/4:
+        return np.nan
+    return np.sqrt(4*x + 7) - 3 * np.cos(x)
 
-def equation_1__(x):
-    return 8 * np.cos(x) - x - 6
+def equation_1_1_fx(x):
+    if x.all() < -7/4:
+        return np.nan
+    return np.sqrt(4*x + 7)
+
+def equation_1_2_fx(x):
+    return 3 * np.cos(x)
 
 def equation_2_fx(x):
-    return x**3 + x**2 + 9
+    return x**3 + 7*x**2 + 1
 
 def equation_1_gx(x):
-    return 8 * np.cos(x) - 6
+    if x < -7/4:
+        return np.nan
+    return np.arccos(np.sqrt(4*x + 7) / 3)
 
 def equation_2_gx(x):
-    return - (x**2 + 9) ** (1/3)
+    return - (7*x**2 + 1) ** (1/3)
 
 def simple_iteration(equation, x0, eps):
     x_n = x0
@@ -30,14 +39,14 @@ def simple_iteration(equation, x0, eps):
     return x_n1, iteration
 
 
-x0_1 = -5
+x0_1 = 0
 eps_equation_1 = 0.0001
-x0_2 = -3
+x0_2 = -6
 eps_equation_2 = 0.01
 
 # Нахождение корней
 root_1_simple, i_1 = simple_iteration(equation_1_gx, x0_1, eps_equation_1)
-root_1_scipy = optimize.root(equation_1__, x0_1).x[0]
+root_1_scipy = optimize.root(equation_1_fx, x0_1).x[0]
 
 root_2_simple, i_2 = simple_iteration(equation_2_gx, x0_2, eps_equation_2)
 root_2_scipy = optimize.root(equation_2_fx, x0_2).x[0]
@@ -58,20 +67,21 @@ else:
 print("Корень 2 уравнения найденный с помощью функции optimize.root: ", root_2_scipy)
 
 # Построение графиков
-x_values_1 = np.linspace(-10, 10, 400)
-y_values_1 = equation_1_fx(x_values_1)
+x_values_1 = np.linspace(-7/4, 5, 400)
+y_values_1_1 = equation_1_1_fx(x_values_1)
+y_values_1_2 = equation_1_2_fx(x_values_1)
 
-x_values_2 = np.linspace(-5, 5, 400)
+x_values_2 = np.linspace(-10, 5, 400)
 y_values_2 = equation_2_fx(x_values_2)
 
 # График для equation_1
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
-plt.plot(x_values_1, y_values_1, label='y = f(x)')
+plt.plot(x_values_1, y_values_1_1, label='y = sqrt(4x+7)')
+plt.plot(x_values_1, y_values_1_2, label='y = 3cos(x)')
 plt.axhline(0, color='red', linewidth=0.5, linestyle='--')
 plt.axvline(0, color='red', linewidth=0.5, linestyle='--')
-plt.scatter([root_1_simple, root_1_scipy], [equation_1_fx(root_1_simple), equation_1_fx(root_1_scipy)], color='red', label='Найденные корни', zorder=5)
-plt.title('График функции y = 8 * cos(x) - x')
+plt.scatter([root_1_simple, root_1_scipy], [equation_1_1_fx(root_1_simple), equation_1_1_fx(root_1_scipy)], color='red', label='Найденные корни', zorder=5)
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid()
@@ -79,11 +89,10 @@ plt.legend()
 
 # График для equation_2
 plt.subplot(1, 2, 2)
-plt.plot(x_values_2, y_values_2, label='y = f(x)')
+plt.plot(x_values_2, y_values_2, label='y = x^3 + 7*x^2 + 1')
 plt.axhline(0, color='red', linewidth=0.5, linestyle='--')
 plt.axvline(0, color='red', linewidth=0.5, linestyle='--')
 plt.scatter([root_2_simple, root_2_scipy], [equation_2_fx(root_2_simple), equation_2_fx(root_2_scipy)], color='red', label='Найденные корни', zorder=5)
-plt.title('График функции y = x^3 + x^2 + 9')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid()
